566da41e117ffdf04b63928ad88f598963f5887e
akaam <akaamil@outlook.com>
2018-11-07 16:39:53 +0300

    login page added is working

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -9,7 +9,7 @@
 
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
     <meta name="viewport" content="width=device-width"/>
-
+//
 
     {{  Html::style('assets/css/bootstrap.min.css') }}
     {{  Html::style('assets/css/animate.min.css') }}
