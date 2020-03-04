475547b7806e328dca75cd68f8a7648d4a0f8c55
DigitalTrade Yazilim <yazilim@digitaltrade.com.tr>
2020-03-04 12:42:07 +0300

    ..

--- config/database.php
+++ config/database.php
@@ -15,8 +15,8 @@ return [
     |
     */
 
-    // 'default' => env('DB_CONNECTION', 'mysql'),
-    'default' => 'pgsql',
+    'default' => env('DB_CONNECTION', 'mysql'),
+    // 'default' => 'pgsql',
 
     /*
     |--------------------------------------------------------------------------
